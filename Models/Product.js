a3f8a16cac64dbebcdffe4a0c0cebec418a5e113
LauraMartinez20 <danielamartinez.1012@gmail.com>
2022-10-03 11:34:20 -0500

    create Models/

---
+++ Models/Product.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const ProductSchema = new mongoose.Schema ({
+    brand: {type: String, required: true},
+    model: {type: String, required: true},
+    description: {type: String, required: true},
+    price: {type: Number, required: true},
+    likes: {type: Array, required: true},
+    stock: {type: Number, required: true},
+    photo: {type: String, required: true},
+})
+
+const Product = mongoose.model(
+    "products",
+    ProductSchema
+)
+
+module.exports = Product
