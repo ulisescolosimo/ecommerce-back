56a42cbe76edc3b3ed33ce983d9f4c9fb1e46d04
ulisescolosimo <colosimo101@gmail.com>
2022-10-04 12:49:48 -0300

    Ruta para verificar email

--- routes/usersLogin.js
+++ routes/usersLogin.js
@@ -2,13 +2,13 @@ var express = require('express');
 var router = express.Router();
 const passport = require("../config/passport");
 
-const {signUp, getUsers, deleteUser, verifyMail, signIn, signOut, verifyToken} = require('../controllers/userController')
+const {signUp, getUsers, deleteUser, verifyMail, signIn, signOut, /* verifyToken */} = require('../controllers/userController')
 
 router.post('/signup', signUp);
 router.post('/signin', signIn)
 router.get('/', getUsers);
 router.get('/verify/:code', verifyMail)
-router.get('/token'/* , passport.authenticate('jwt', {session:false}) */, verifyToken);
+/* router.get('/token' , passport.authenticate('jwt', {session:false}), verifyToken); */
 router.patch('/:id', signOut)
 router.delete('/:id', deleteUser)
 
