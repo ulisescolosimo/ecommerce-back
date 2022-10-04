960730ed11757cc6f87d17c416c3d4ea18178a4d
GuidoSeia <guidoseia5@gmail.comguidoseia5@gmail.com>
2022-10-04 11:19:20 -0300

    agregado de requerimientos al modelo products

--- Models/Product.js
+++ Models/Product.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose')
 const ProductSchema = new mongoose.Schema ({
     brand: {type: String, required: true},
     model: {type: String, required: true},
-    description: {type: String, required: true},
-    price: {type: Number, required: true},
+    description: {type: String, required: true, min: 5, max: 300},
+    price: {type: Number, required: true, min: 1},
     likes: {type: Array},
-    type: {type:String},
+    type: {type: String, required: true},
     stock: {type: Number, required: true},
     photo: {type: Array, required: true},
 })
