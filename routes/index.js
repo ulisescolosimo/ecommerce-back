0846336279a861947150db0056f6001f37f8dad4
GuidoSeia <guidoseia5@gmail.comguidoseia5@gmail.com>
2022-10-07 18:33:16 -0300

    agregado ded ruta

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 const usersRouter = require('./usersLogin')
 const productsRouter = require('./products')
+const reviewsRouter = require('./reviews')
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'The Salty Shop' });
@@ -10,5 +11,6 @@ router.get('/', function(req, res, next) {
 
 router.use('/auth', usersRouter)
 router.use('/products', productsRouter)
+router.use('/reviews', reviewsRouter)
 
 module.exports = router;
