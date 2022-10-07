d6b74a6d23c112f6cdc3178d00edcc26b78e91ae
engelberthRetamal <engel.retamal.e@gmail.com>
2022-10-06 21:46:05 -0300

    Se creó la ruta de coupons y se agregó a index en routes

---
+++ routes/coupons.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+const { getAllCoupons ,getCoupon, createCoupon, deleteCoupon } = require('../controllers/couponController')
+
+router.get('/', getAllCoupons)
+router.get('/:id', getCoupon)
+router.post('/', createCoupon);
+router.delete('/:id', deleteCoupon);
+
+module.exports = router;
