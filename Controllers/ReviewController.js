f7d5690c26357627eeb442ad1cca50572e1c8ea0
your name <your github email address>
2022-10-10 14:06:26 -0300

    Controlador review all

--- Controllers/ReviewController.js
+++ Controllers/ReviewController.js
@@ -37,12 +37,12 @@ const ReviewController = {
                     response: ReviewOne,
                     success: true
                 })
-             } else {
+            } else {
                 res.status(404).json({
                     message: "couldn't find review",
                     success: false
                 })
-             }
+            }
         } catch (error) {
             console.log(error)
             res.status(400).json({
@@ -51,40 +51,35 @@ const ReviewController = {
             })   
         }
     },
-    reviews: async (res,req) => {
-        let {id} = req.params
-        let query={}
-
-        if (req.query.user) {
-            query.user = req.query.user
-        } 
-        if (req.query.product) {
-        query.product = req.query.product
+    getReviews: async (req, res) => {
+    
+        let query = {};
+    
+        if (req.query.type) {
+        query.type = req.query.type;
         }
+    
+        try {
+            let reviews = await Review.find(query)
+                .populate('user', { name: 1, photo: 1 })
+                .populate('product', {brand: 1, model: 1, photo: 1})
 
-    try {
-        let reviewNew = await Review.find(query)
-        .populate("user", {name:1, photo:1, _id:1})
-        .populate("product", {name:1, photo:1, _id:1})
-        if (reviewNew) {
+        if (reviews) {
             res.status(200).json({
-                message: 'You get reviews',
-                response: reviewNew,
-                success: true
-            })
-        } else{
+            message: "you get all the reviews",
+            response: reviews,
+            success: true,
+            });
+        } else {
             res.status(404).json({
-                message: "Could't find reviews",
-                success: false
-            })
+            message: "could't find all the reviews",
+            success: false,
+            });
         }
-    } catch (error) {
-        res.status(400).json({
-            message: "error, try again",
-            success: false
-        })
-    }
-},
+        } catch (error) {
+        console.log(error);
+        }
+    },
 
     createReview: async (req, res) => {
         let user = req.user.id
@@ -145,25 +140,6 @@ const ReviewController = {
          console.log(error)   
         }
     },
-    
-  /*  destroy: async (req, res) => {
-        const Revieww = req.body
-        let { id } = req.params
-        try {
-            let deleteReview = await Review.findOneAndDelete({ _id: id }, Revieww,)
-            res.status(200).json({
-                message: "Review deleted",
-                success: true
-            })
-
-        } catch (error) {
-            console.log(error)
-            res.status(400).json({
-                message: "Review not found",
-                success: false
-            })
-        }
-    }*/
 }
 
 module.exports = ReviewController
