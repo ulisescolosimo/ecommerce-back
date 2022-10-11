4fe9f8c460053eedaccaa4b3f167d2a17570f12a
LauraMartinez20 <danielamartinez.1012@gmail.com>
2022-10-11 17:14:03 -0500

    added favorites router

--- routes/products.js
+++ routes/products.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var router = express.Router();
+let passport = require('../config/passport')
 
-const { getProducts, getOneProduct, createProduct, updateProduct, deleteProduct } = require('../controllers/productController')
+const { getProducts, getOneProduct, createProduct, updateProduct, deleteProduct, favorites } = require('../controllers/productController')
 
 router.get('/', getProducts)
 router.get('/:id', getOneProduct)
 router.post('/', createProduct);
 router.delete('/:id', deleteProduct);
+router.patch('/favorite/:id', passport.authenticate('jwt', {session:false}), favorites)
 router.patch('/:id', updateProduct);
 
 module.exports = router;
